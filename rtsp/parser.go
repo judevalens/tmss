72ec4413f3013df78070c1fe23c92b72bf18492a
Jude <judevalens@gmail.com>
2022-10-18 22:52:27 -0400

    current state

--- rtsp/parser.go
+++ rtsp/parser.go
@@ -1,8 +1,11 @@
-package rtsp
+package rtsp_parser
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
+	"net"
 	"net/url"
 	"strconv"
 	"strings"
@@ -11,13 +14,25 @@ import (
 const LineBreak = "\r\n"
 
 type RtspRequest struct {
-	Method     string
-	Uri        *url.URL
-	Version    string
-	Headers    map[string]string
-	headerSize int
-	BodySize   int
-	Body       string
+	Method  string
+	Uri     *url.URL
+	Version string
+	Headers map[string]string
+	Body    []byte
+}
+
+type Transport struct {
+	Append      bool
+	IsUnicast   bool
+	Destination net.Addr
+	Interleaved string
+	TTL         int
+	Layers      int
+	Port        string
+	ClientPort  string
+	ServerPort  string
+	Ssrc        string
+	Mode        string
 }
 
 type RtspResponse struct {
@@ -25,19 +40,20 @@ type RtspResponse struct {
 	StatusCode int
 	Reason     string
 	Headers    map[string]string
-	BodySize   int
-	headerSize int
 }
 
-func ParseRequest(msg string) (RtspRequest, error) {
+func ParseRequest(reader io.Reader) (RtspRequest, error) {
 	rtpMsg := RtspRequest{
 		Headers: map[string]string{
 			"Content-Length": "0",
 		},
 	}
 	var i = 0
-	currentLine, i := readline(msg, i)
-
+	bufferedReader := bufio.NewReader(reader)
+	currentLine, err := readline(bufferedReader)
+	if err != nil {
+		return RtspRequest{}, err
+	}
 	fmt.Printf("new index: %v\n", i)
 	statusLine := strings.Split(currentLine, " ")
 	fmt.Printf("status line: %v\n", statusLine)
@@ -51,26 +67,38 @@ func ParseRequest(msg string) (RtspRequest, error) {
 	}
 	rtpMsg.Uri = u
 	rtpMsg.Version = statusLine[2]
-	i, err = parseHeader(msg, i, rtpMsg.Headers)
+	err = parseHeader(bufferedReader, rtpMsg.Headers)
 	if err != nil {
 		fmt.Printf("incorrect header line")
 		return RtspRequest{}, err
 	}
-	bodySize, err := strconv.Atoi(rtpMsg.Headers["Content-length"])
+
+	// check if for request body
+
+	bodyLen, err := strconv.Atoi(rtpMsg.Headers["Content-Length"])
 	if err != nil {
-		fmt.Printf("invalid header")
+		fmt.Println("Invalid content length")
 		return RtspRequest{}, err
 	}
-	rtpMsg.BodySize = bodySize
+
+	rtpMsg.Body = make([]byte, bodyLen)
+
+	_, err = bufferedReader.Read(rtpMsg.Body)
+	if err != nil {
+		fmt.Println("Failed to read request body")
+		return RtspRequest{}, err
+	}
+
 	return rtpMsg, nil
 }
 
-func ParseResponse(msg string) (RtspResponse, error) {
+func ParseResponse(reader io.Reader) (RtspResponse, error) {
 	rtspResponse := RtspResponse{
 		Headers: map[string]string{},
 	}
-	i := 0
-	currentLine, i := readline(msg, i)
+
+	bufferedReader := bufio.NewReader(reader)
+	currentLine, err := readline(bufferedReader)
 	statusLine := strings.Split(currentLine, " ")
 	if len(statusLine) != 3 {
 		return RtspResponse{}, errors.New("too many item in status line")
@@ -82,7 +110,7 @@ func ParseResponse(msg string) (RtspResponse, error) {
 	}
 	rtspResponse.StatusCode = statusCode
 	rtspResponse.Reason = statusLine[2]
-	_, err = parseHeader(msg, i, rtspResponse.Headers)
+	err = parseHeader(bufferedReader, rtspResponse.Headers)
 	if err != nil {
 		fmt.Printf("incorrect header line")
 		return RtspResponse{}, err
@@ -90,31 +118,38 @@ func ParseResponse(msg string) (RtspResponse, error) {
 	return rtspResponse, nil
 }
 
-func parseHeader(msg string, startIndex int, headerMap map[string]string) (int, error) {
-	startIndex2 := startIndex
-	for currentLine, startIndex2 := readline(msg, startIndex2); currentLine != "\r\n"; currentLine, startIndex2 = readline(msg, startIndex2) {
+func parseHeader(reader *bufio.Reader, headers map[string]string) error {
+	currentLine, err := readline(reader)
+	for currentLine != "" && err == nil {
 		key, value, found := strings.Cut(currentLine, ":")
 		/// check that grammar is valid
 		if !found && currentLine != LineBreak {
 			fmt.Printf("incorrect header line: %v\n", currentLine)
-			return startIndex2, errors.New("incorrect header line")
+			return errors.New("incorrect header line")
 		}
 		if found {
-			headerMap[key] = value
+			key = strings.TrimSpace(key)
+			value = strings.TrimSpace(value)
+			headers[key] = value
 		}
+		currentLine, err = readline(reader)
 	}
-	return startIndex2, nil
+	return nil
 }
 
-func readline(msg string, i int) (string, int) {
-	i2 := strings.Index(msg[i:], LineBreak)
-	if i2 < 0 {
-		return msg, i2
-	}
-	absEndOfLine := i + i2
-	//line is crlf
-	if absEndOfLine == i {
-		return msg[i : absEndOfLine+len(LineBreak)], absEndOfLine + len(LineBreak)
+func readline(reader *bufio.Reader) (string, error) {
+	var buffer string
+	for {
+		readString, err := reader.ReadString('\n')
+		if err != nil {
+			buffer += readString
+			return buffer, err
+		}
+		buffer += readString
+
+		if buffer[len(buffer)-2:] == "\r\n" {
+			return buffer[:len(buffer)-2], nil
+		}
 	}
 	fmt.Printf("new line: from %v->%v, %v\n", i, absEndOfLine, msg[i:absEndOfLine])
 	fmt.Printf("EOL\n")
